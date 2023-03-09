e331b2e5b8f6ea9da66203782b5dc384c8c2afc3
Julian Quispe <julianmql@hotmail.com>
2023-03-09 01:29:17 -0500

    feat: schedule generated with chrome storage

--- src/content.ts
+++ src/content.ts
@@ -27,8 +27,19 @@ function searchBloque (
 function goToSchedule(){
     chrome.tabs.create({ url: chrome.runtime.getURL("options.html") });
 }
-
+  
 const getDocumentInfo = (nombre: string) => {
+
+    const approxTimes = (timeString: string): number[] =>{
+        const times = timeString.split('-'); // separar los tiempos
+        const start = times[0].split(':').map(Number); // convertir a números
+        const end = times[1].split(':').map(Number);
+        const startHour = start[1] >= 40 ? start[0] + 1 : start[0]; // aproximar horas de inicio
+        const endHour = end[1] >= 40 ? end[0] + 1 : end[0]; // aproximar horas de fin
+        const endArr = Array.from({ length: endHour - startHour }, (_, i) => startHour + i); // generar array de horas de fin
+        return endArr;
+    }
+
     let n_nombre_bloque = nombre.trim()+'\u00A0';
     let frame : any = document.querySelector('frame[name="ficha Matricula"]');
     let bloque : any = Array.from(frame.contentDocument.getElementsByTagName('font')).filter((el: any)  => el.textContent.trim() === n_nombre_bloque.trim());
@@ -43,7 +54,8 @@ const getDocumentInfo = (nombre: string) => {
     let btag = fonttag.firstElementChild;
     let nombreCurso = btag.textContent.trim();
     let horarios : any = {
-        "nombreCurso" : nombreCurso
+        "nombreCurso" : nombreCurso,
+        "frecuencia" : rowspan
     }
     let row = bloquetr;
     for (let i = 0; i < rowspan; i++) {
@@ -51,16 +63,27 @@ const getDocumentInfo = (nombre: string) => {
         {
             row = bloquetr.nextElementSibling;
             horarios["dia"+i] = row.cells[2].textContent.trim();
-            horarios["hora"+i] = row.cells[4].textContent.trim();
+            horarios["hora"+i] = approxTimes(row.cells[4].textContent.trim());
         }
         else{
             horarios["dia"+i] = row.cells[5].textContent.trim();
-            horarios["hora"+i] = row.cells[7].textContent.trim();
+            horarios["hora"+i] = approxTimes(row.cells[7].textContent.trim());
         }
         bloquetr = row;
     }
-    chrome.storage.local.set({ nombreCurso : horarios }, function(){
-    });
+    let addCursos = [horarios]
+    chrome.storage.local.get(["cursos"]).then((result) => {
+        if (result.cursos){
+            addCursos = result.cursos;
+            addCursos.push(horarios);
+            chrome.storage.local.set({ cursos: addCursos }).then(() => {
+            });
+        }
+        else{
+            chrome.storage.local.set({ cursos: addCursos }).then(() => {
+              });
+        }
+      });
     chrome.runtime.sendMessage({method: "set", value: nombreCurso }, () => {});
   }
 
--- src/views/Components/Schedule/schedule.tsx
+++ src/views/Components/Schedule/schedule.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Table} from '@mantine/core';
 
 type ScheduleItem = {
+    id: number;
   time: string;
   monday: string;
   tuesday: string;
@@ -12,55 +13,109 @@ type ScheduleItem = {
   sunday: string;
 }
 
-const schedule: ScheduleItem[] = [
-  { time: '7:00am', monday: '', tuesday: '', wednesday: '', thursday: '', friday: '', saturday: '', sunday: '' },
-  { time: '8:00am', monday: '', tuesday: '', wednesday: '', thursday: '', friday: '', saturday: '', sunday: '' },
-  { time: '9:00am', monday: '', tuesday: '', wednesday: '', thursday: '', friday: '', saturday: '', sunday: '' },
-  { time: '10:00am', monday: '', tuesday: '', wednesday: '', thursday: '', friday: '', saturday: '', sunday: '' },
-  { time: '11:00am', monday: '', tuesday: '', wednesday: '', thursday: '', friday: '', saturday: '', sunday: '' },
-  { time: '12:00pm', monday: '', tuesday: '', wednesday: '', thursday: '', friday: '', saturday: '', sunday: '' },
-  { time: '1:00pm', monday: '', tuesday: '', wednesday: '', thursday: '', friday: '', saturday: '', sunday: '' },
-  { time: '2:00pm', monday: '', tuesday: '', wednesday: '', thursday: '', friday: '', saturday: '', sunday: '' },
-  { time: '3:00pm', monday: '', tuesday: '', wednesday: '', thursday: '', friday: '', saturday: '', sunday: '' },
-  { time: '4:00pm', monday: '', tuesday: '', wednesday: '', thursday: '', friday: '', saturday: '', sunday: '' },
-  { time: '5:00pm', monday: '', tuesday: '', wednesday: '', thursday: '', friday: '', saturday: '', sunday: '' },
-  { time: '6:00pm', monday: '', tuesday: '', wednesday: '', thursday: '', friday: '', saturday: '', sunday: '' },
-  { time: '7:00pm', monday: '', tuesday: '', wednesday: '', thursday: '', friday: '', saturday: '', sunday: '' },
-  { time: '8:00pm', monday: '', tuesday: '', wednesday: '', thursday: '', friday: '', saturday: '', sunday: '' },
-  { time: '9:00pm', monday: '', tuesday: '', wednesday: '', thursday: '', friday: '', saturday: '', sunday: '' },
-  { time: '10:00pm', monday: '', tuesday: '', wednesday: '', thursday: '', friday: '', saturday: '', sunday: '' },
-  { time: '11:00pm', monday: '', tuesday: '', wednesday: '', thursday: '', friday: '', saturday: '', sunday: '' },
-  { time: '12:00am', monday: '', tuesday: '', wednesday: '', thursday: '', friday: '', saturday: '', sunday: '' },
+
+let schedule: ScheduleItem[] = [
+  { id: 7, time: '7:00am', monday: '', tuesday: '', wednesday: '', thursday: '', friday: '', saturday: '', sunday: '' },
+  { id: 8, time: '8:00am', monday: '', tuesday: '', wednesday: '', thursday: '', friday: '', saturday: '', sunday: '' },
+  { id: 9, time: '9:00am', monday: '', tuesday: '', wednesday: '', thursday: '', friday: '', saturday: '', sunday: '' },
+  { id: 10, time: '10:00am', monday: '', tuesday: '', wednesday: '', thursday: '', friday: '', saturday: '', sunday: '' },
+  { id: 11, time: '11:00am', monday: '', tuesday: '', wednesday: '', thursday: '', friday: '', saturday: '', sunday: '' },
+  { id: 12, time: '12:00pm', monday: '', tuesday: '', wednesday: '', thursday: '', friday: '', saturday: '', sunday: '' },
+  { id: 13, time: '13:00pm', monday: '', tuesday: '', wednesday: '', thursday: '', friday: '', saturday: '', sunday: '' },
+  { id: 14, time: '14:00pm', monday: '', tuesday: '', wednesday: '', thursday: '', friday: '', saturday: '', sunday: '' },
+  { id: 15, time: '15:00pm', monday: '', tuesday: '', wednesday: '', thursday: '', friday: '', saturday: '', sunday: '' },
+  { id: 16, time: '16:00pm', monday: '', tuesday: '', wednesday: '', thursday: '', friday: '', saturday: '', sunday: '' },
+  { id: 17, time: '17:00pm', monday: '', tuesday: '', wednesday: '', thursday: '', friday: '', saturday: '', sunday: '' },
+  { id: 18, time: '18:00pm', monday: '', tuesday: '', wednesday: '', thursday: '', friday: '', saturday: '', sunday: '' },
+  { id: 19, time: '19:00pm', monday: '', tuesday: '', wednesday: '', thursday: '', friday: '', saturday: '', sunday: '' },
+  { id: 20, time: '20:00pm', monday: '', tuesday: '', wednesday: '', thursday: '', friday: '', saturday: '', sunday: '' },
+  { id: 21, time: '21:00pm', monday: '', tuesday: '', wednesday: '', thursday: '', friday: '', saturday: '', sunday: '' },
+  { id: 22, time: '22:00pm', monday: '', tuesday: '', wednesday: '', thursday: '', friday: '', saturday: '', sunday: '' },
+  { id: 23, time: '23:00pm', monday: '', tuesday: '', wednesday: '', thursday: '', friday: '', saturday: '', sunday: '' },
+  { id: 24, time: '00:00am', monday: '', tuesday: '', wednesday: '', thursday: '', friday: '', saturday: '', sunday: '' },
 ];
 
 const WeekSchedule = () => {
+
+    const [scheduleCompleted, setScheduleCompleted] = React.useState<ScheduleItem[]>([]);
+
+    React.useEffect(() => {
+        let newSchedule = schedule;
+        chrome.storage.local.get(["cursos"]).then((result) => {
+            if (result.cursos){
+                let cursos = result.cursos;
+                console.log(cursos);
+                for (let i = 0; i < cursos.length; i++) {
+                    let curso = cursos[i];
+                    let frecuencia = curso.frecuencia;
+                    for (let j = 0; j < frecuencia; j++) {
+                        let dia : keyof ScheduleItem = 'monday';
+                        let diaString = 'dia' + j;
+                        switch(curso[diaString]){
+                            case "Lunes":
+                                dia = 'monday';
+                                break;
+                            case "Martes":
+                                dia = 'tuesday';
+                                break;
+                            case "Miércoles":
+                                dia = 'wednesday';
+                                break;
+                            case "Jueves":
+                                dia = 'thursday';
+                                break;
+                            case "Viernes":
+                                dia = 'friday';
+                                break;
+                            case "Sábado":
+                                dia = 'saturday';
+                                break;
+                            case "Domingo":
+                                dia = 'sunday';
+                                break;
+                        }
+                        let horaString = 'hora' + j;
+                        let horas = curso[horaString];
+                        for (let k = 0; k < horas.length; k++){
+                            let horasindex : number = horas[k] - 7;
+                            newSchedule[horasindex][dia] = 'True';
+                        }
+    
+                    }
+                }
+            }
+          });
+          setScheduleCompleted([...newSchedule]);
+        });
+    
+
   return (
-    <div style={{ maxWidth: "1000px", overflowX: "auto" }}>
+    <div style={{ width: "900px", overflowX: "auto" }}>
     <Table highlightOnHover withBorder withColumnBorders  
     style={{ backgroundColor: "#ffffff", margin: "auto"}}
     
     >
     <thead>
     <tr>
-    <th style={{ width: '10%' }}>Time</th>
-    <th style={{ width: '15%' }}>Monday</th>
-    <th style={{ width: '15%' }}>Tuesday</th>
-    <th style={{ width: '15%' }}>Wednesday</th>
-    <th style={{ width: '15%' }}>Thursday</th>
-    <th style={{ width: '15%' }}>Friday</th>
-    <th style={{ width: '15%' }}>Saturday</th>
-    <th style={{ width: '15%' }}>Sunday</th>
+    <th style={{ width: '10%' }}>Hora</th>
+    <th style={{ width: '15%' }}>Lunes</th>
+    <th style={{ width: '15%' }}>Martes</th>
+    <th style={{ width: '15%' }}>Miercoles</th>
+    <th style={{ width: '15%' }}>Jueves</th>
+    <th style={{ width: '15%' }}>Viernes</th>
+    <th style={{ width: '15%' }}>Sabado</th>
+    <th style={{ width: '15%' }}>Domingo</th>
     </tr>
     </thead>
     <tbody>
-    {schedule.map((item, index) => (
+    {scheduleCompleted.map((item, index) => (
     <tr key={index}>
     <td>{item.time}</td>
     <td>{item.monday}</td>
     <td>{item.tuesday}</td>
     <td>{item.wednesday}</td>
     <td>{item.thursday}</td>
-    <td>{item.friday}</td>
+    <td style={{ backgroundColor: item.friday == 'True' ? 'red' : 'white' }}></td>
     <td>{item.saturday}</td>
     <td>{item.sunday}</td>
     </tr>
