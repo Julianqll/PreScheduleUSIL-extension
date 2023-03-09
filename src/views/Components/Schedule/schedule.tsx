89fd6336a744a138905c322be4b2ec2d673ed1b5
Julian Quispe <julianmql@hotmail.com>
2023-03-09 17:31:16 -0500

    feat: styles & minimal fixes added

--- src/views/Components/Schedule/schedule.tsx
+++ src/views/Components/Schedule/schedule.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Table} from '@mantine/core';
+import { ColorSwatch, Flex, Group, Table, Text} from '@mantine/core';
 
 type ScheduleItem = {
     id: number;
@@ -57,17 +57,35 @@ let schedule: ScheduleItem[] = [
   { id: 24, time: '00:00am', monday: '', tuesday: '', wednesday: '', thursday: '', friday: '', saturday: '', sunday: '' },
 ];
 
+type CursoItem ={
+    id: string;
+    nombre: string;
+    color: string;
+}
+
 const WeekSchedule = () => {
+
+    const [cursosL, setCursosL] = React.useState<CursoItem[]>([]);
 
     const [scheduleCompleted, setScheduleCompleted] = React.useState<ScheduleItem[]>([]);
 
     React.useEffect(() => {
-        let newSchedule = schedule;
+
+        let newSchedule = [...schedule];
         chrome.storage.local.get(["cursos"]).then((result) => {
             if (result.cursos){
                 let cursos = result.cursos;
                 console.log(cursos);
                 for (let i = 0; i < cursos.length; i++) {
+                    let nuevoCurso : CursoItem = {
+                        id: '',
+                        nombre: '',
+                        color: '',
+                    };
+                    nuevoCurso.id = cursos[i].bloqueId;
+                    nuevoCurso.nombre = cursos[i].nombreCurso;
+                    nuevoCurso.color = colors[i];
+                    setCursosL([...cursosL, nuevoCurso]);
                     let curso = cursos[i];
                     let color = colors[i];
                     let frecuencia = curso.frecuencia;
@@ -117,47 +135,87 @@ const WeekSchedule = () => {
     
                     }
                 }
+                setScheduleCompleted(newSchedule);
             }
           });
-          setScheduleCompleted([...newSchedule]);
-        });
+          
+        },[]);
     
-
   return (
-    <div style={{ width: "900px", overflowX: "auto" }}>
-    <Table highlightOnHover withBorder withColumnBorders  
-    style={{ backgroundColor: "#ffffff", margin: "auto"}}
-    
+    <Flex
+    mih={50}
+    gap="md"
+    justify="center"
+    align="center"
+    direction="row"
+    wrap="wrap"
     >
-    <thead>
-    <tr>
-    <th style={{ width: '10%' }}>Hora</th>
-    <th style={{ width: '15%' }}>Lunes</th>
-    <th style={{ width: '15%' }}>Martes</th>
-    <th style={{ width: '15%' }}>Miercoles</th>
-    <th style={{ width: '15%' }}>Jueves</th>
-    <th style={{ width: '15%' }}>Viernes</th>
-    <th style={{ width: '15%' }}>Sabado</th>
-    <th style={{ width: '15%' }}>Domingo</th>
-    </tr>
-    </thead>
-    <tbody>
-    {scheduleCompleted.map((item, index) => (
-    <tr key={index}>
-    <td>{item.time}</td>
-    <td style={{ backgroundColor: item.monday == 'True' ? item.color : 'white' }}>{item.mondayData}</td>
-    <td style={{ backgroundColor: item.tuesday == 'True' ? item.color : 'white' }}>{item.tuesdayData}</td>
-    <td style={{ backgroundColor: item.wednesday == 'True' ? item.color : 'white' }}>{item.wednesdayData}</td>
-    <td style={{ backgroundColor: item.thursday == 'True' ? item.color : 'white' }}>{item.thursdayData}</td>
-    <td style={{ backgroundColor: item.friday == 'True' ? item.color : 'white' }}>{item.fridayData}</td>
-    <td style={{ backgroundColor: item.saturday == 'True' ? item.color : 'white' }}>{item.saturdayData}</td>
-    <td style={{ backgroundColor: item.sunday == 'True' ? item.color : 'white' }}>{item.sundayData}</td>
-    </tr>
-    ))}
-    </tbody>
-    </Table>
-    </div>
+        <div
+        style={{
+            maxWidth: '90vw',
+            maxHeight: '100vh',
+            overflowX: 'auto',
+            margin: '0 auto',
+        }}
+        >
+            <Table highlightOnHover withBorder withColumnBorders  
+            style={{ backgroundColor: "#ffffff",  width: '100%'}}
+            >
+                <thead>
+                <tr>
+                <th style={{ width: '10%', textAlign: 'center' }}>Hora</th>
+                <th style={{ width: '15%', textAlign: 'center' }}>Lunes</th>
+                <th style={{ width: '15%', textAlign: 'center' }}>Martes</th>
+                <th style={{ width: '15%', textAlign: 'center' }}>Miercoles</th>
+                <th style={{ width: '15%', textAlign: 'center' }}>Jueves</th>
+                <th style={{ width: '15%', textAlign: 'center' }}>Viernes</th>
+                <th style={{ width: '15%', textAlign: 'center' }}>Sabado</th>
+                <th style={{ width: '15%', textAlign: 'center' }}>Domingo</th>
+                </tr>
+                </thead>
+                <tbody>
+                {scheduleCompleted.map((item, index) => (
+                <tr key={index}>
+                <td>{item.time}</td>
+                <td style={{ backgroundColor: item.monday == 'True' ? item.color : 'white' }}>{item.mondayData}</td>
+                <td style={{ backgroundColor: item.tuesday == 'True' ? item.color : 'white' }}>{item.tuesdayData}</td>
+                <td style={{ backgroundColor: item.wednesday == 'True' ? item.color : 'white' }}>{item.wednesdayData}</td>
+                <td style={{ backgroundColor: item.thursday == 'True' ? item.color : 'white' }}>{item.thursdayData}</td>
+                <td style={{ backgroundColor: item.friday == 'True' ? item.color : 'white' }}>{item.fridayData}</td>
+                <td style={{ backgroundColor: item.saturday == 'True' ? item.color : 'white' }}>{item.saturdayData}</td>
+                <td style={{ backgroundColor: item.sunday == 'True' ? item.color : 'white' }}>{item.sundayData}</td>
+                </tr>
+                ))}
+                </tbody>
+            </Table>
+        </div>
+        <Flex
+        mih={50}
+        gap="md"
+        justify="center"
+        align="center"
+        direction="column"
+        wrap="wrap"
+        >          
+            <Text
+                c="blue"
+                sx={{ fontFamily: 'Greycliff CF, sans-serif' }}
+                ta="center"
+                fz={30}
+                fw={700}
+            >
+                USIL Pre-horario
+            </Text>
+            {cursosL.map((item, index) => (
+                <Group>
+                    <Text fz="md">{item.nombre}</Text>
+                    <Text fz="md">({item.id})</Text>
+                    <ColorSwatch key={index} color={item.color} />
+                </Group>
+            ))}
+        </Flex>
+    </Flex>
     );
-    };
+};
     
 export default WeekSchedule;
